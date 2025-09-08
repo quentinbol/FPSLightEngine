c6027460b77bd1ad265ac17d24ecea389abb973e
quentinbol <quentin.bollore@epitech.eu>
2025-09-08 00:58:40 -0400

    feat: Refactor Mesh component to include makeCube and makePyramid methods with proper buffer setup

--- src/ecs/component/MeshComponent.hpp
+++ src/ecs/component/MeshComponent.hpp
@@ -26,74 +26,77 @@ struct Mesh {
         return (maxBounds - minBounds) * 0.5f;
     }
 
-    static Mesh makeCube() {
-        float vertices[] = {
-            -0.5f, -0.5f, -0.5f,   0, 0, -1,   0, 0,
-             0.5f, -0.5f, -0.5f,   0, 0, -1,   1, 0,
-             0.5f,  0.5f, -0.5f,   0, 0, -1,   1, 1,
-            -0.5f,  0.5f, -0.5f,   0, 0, -1,   0, 1,
+   static Mesh makeCube() {
+    float vertices[] = {
+        -0.5f, -0.5f,  0.5f,  0, 0, 1,  0, 0,
+         0.5f, -0.5f,  0.5f,  0, 0, 1,  1, 0,
+         0.5f,  0.5f,  0.5f,  0, 0, 1,  1, 1,
+        -0.5f,  0.5f,  0.5f,  0, 0, 1,  0, 1,
 
-            -0.5f, -0.5f,  0.5f,   0, 0, 1,   0, 0,
-             0.5f, -0.5f,  0.5f,   0, 0, 1,   1, 0,
-             0.5f,  0.5f,  0.5f,   0, 0, 1,   1, 1,
-            -0.5f,  0.5f,  0.5f,   0, 0, 1,   0, 1,
+        -0.5f, -0.5f, -0.5f,  0, 0, -1, 0, 0,
+         0.5f, -0.5f, -0.5f,  0, 0, -1, 1, 0,
+         0.5f,  0.5f, -0.5f,  0, 0, -1, 1, 1,
+        -0.5f,  0.5f, -0.5f,  0, 0, -1, 0, 1,
 
-            -0.5f,  0.5f,  0.5f,  -1, 0, 0,   1, 0,
-            -0.5f,  0.5f, -0.5f,  -1, 0, 0,   1, 1,
-            -0.5f, -0.5f, -0.5f,  -1, 0, 0,   0, 1,
-            -0.5f, -0.5f,  0.5f,  -1, 0, 0,   0, 0,
+        -0.5f, -0.5f, -0.5f, -1, 0, 0,  0, 0,
+        -0.5f,  0.5f, -0.5f, -1, 0, 0,  1, 0,
+        -0.5f,  0.5f,  0.5f, -1, 0, 0,  1, 1,
+        -0.5f, -0.5f,  0.5f, -1, 0, 0,  0, 1,
 
-             0.5f,  0.5f,  0.5f,   1, 0, 0,   1, 0,
-             0.5f,  0.5f, -0.5f,   1, 0, 0,   1, 1,
-             0.5f, -0.5f, -0.5f,   1, 0, 0,   0, 1,
-             0.5f, -0.5f,  0.5f,   1, 0, 0,   0, 0,
+         0.5f, -0.5f, -0.5f,  1, 0, 0,  0, 0,
+         0.5f,  0.5f, -0.5f,  1, 0, 0,  1, 0,
+         0.5f,  0.5f,  0.5f,  1, 0, 0,  1, 1,
+         0.5f, -0.5f,  0.5f,  1, 0, 0,  0, 1,
 
-            -0.5f, -0.5f, -0.5f,   0, -1, 0,   0, 1,
-             0.5f, -0.5f, -0.5f,   0, -1, 0,   1, 1,
-             0.5f, -0.5f,  0.5f,   0, -1, 0,   1, 0,
-            -0.5f, -0.5f,  0.5f,   0, -1, 0,   0, 0,
+        -0.5f, -0.5f, -0.5f,  0, -1, 0, 0, 0,
+         0.5f, -0.5f, -0.5f,  0, -1, 0, 1, 0,
+         0.5f, -0.5f,  0.5f,  0, -1, 0, 1, 1,
+        -0.5f, -0.5f,  0.5f,  0, -1, 0, 0, 1,
 
-            -0.5f,  0.5f, -0.5f,   0,  1, 0,   0, 1,
-             0.5f,  0.5f, -0.5f,   0,  1, 0,   1, 1,
-             0.5f,  0.5f,  0.5f,   0,  1, 0,   1, 0,
-            -0.5f,  0.5f,  0.5f,   0,  1, 0,   0, 0
-        };
+        -0.5f,  0.5f, -0.5f,  0, 1, 0, 0, 0,
+         0.5f,  0.5f, -0.5f,  0, 1, 0, 1, 0,
+         0.5f,  0.5f,  0.5f,  0, 1, 0, 1, 1,
+        -0.5f,  0.5f,  0.5f,  0, 1, 0, 0, 1
+    };
 
-        unsigned int indices[] = {
-            0, 1, 2, 2, 3, 0,
-            4, 5, 6, 6, 7, 4,
-            8, 9, 10, 10, 11, 8,
-            12, 13, 14, 14, 15, 12,
-            16, 17, 18, 18, 19, 16,
-            20, 21, 22, 22, 23, 20
-        };
+    unsigned int indices[] = {
+        0,1,2, 2,3,0,
+        4,5,6, 6,7,4,
+        8,9,10, 10,11,8,
+        12,13,14, 14,15,12,
+        16,17,18, 18,19,16, 
+        20,21,22, 22,23,20
+    };
 
-        unsigned int VAO,VBO,EBO;
-        glGenVertexArrays(1,&VAO);
-        glGenBuffers(1,&VBO);
-        glGenBuffers(1,&EBO);
+    unsigned int VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
-        glBindVertexArray(VAO);
+    glBindVertexArray(VAO);
 
-        glBindBuffer(GL_ARRAY_BUFFER,VBO);
-        glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)0);
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(3*sizeof(float)));
-        glEnableVertexAttribArray(1);
-        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(6*sizeof(float)));
-        glEnableVertexAttribArray(2);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
-        glBindVertexArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
-        return Mesh(VAO,VBO,EBO,sizeof(indices)/sizeof(unsigned int),
-            glm::vec3(-0.5f, -0.5f, -0.5f),
-            glm::vec3(0.5f,  0.5f,  0.5f));
-    }
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    glBindVertexArray(0);
+
+    return Mesh(VAO, VBO, EBO, sizeof(indices) / sizeof(unsigned int),
+                glm::vec3(-0.5f,-0.5f,-0.5f),
+                glm::vec3(0.5f,0.5f,0.5f));
+}
+
 
     static Mesh makePlane(int cols, int rows) {
         std::vector<float> vertices;
@@ -159,4 +162,67 @@ struct Mesh {
             glm::vec3(cols / 2.0f, 0.0f, rows / 2.0f));
     }
 
+    static Mesh makePyramid() {
+        float vertices[] = {
+        
+            -0.5f, 0.0f, -0.5f,   0.0f, -1.0f, 0.0f,   0.0f, 0.0f,
+            0.5f, 0.0f, -0.5f,   0.0f, -1.0f, 0.0f,   1.0f, 0.0f,
+            0.5f, 0.0f,  0.5f,   0.0f, -1.0f, 0.0f,   1.0f, 1.0f,
+            -0.5f, 0.0f,  0.5f,   0.0f, -1.0f, 0.0f,   0.0f, 1.0f,
+
+            -0.5f, 0.0f, -0.5f,   0.0f, 0.707f, -0.707f,  0.0f, 0.0f,
+            0.5f, 0.0f, -0.5f,   0.0f, 0.707f, -0.707f,  1.0f, 0.0f,
+            0.0f, 1.0f,  0.0f,   0.0f, 0.707f, -0.707f,  0.5f, 1.0f,
+
+            0.5f, 0.0f, -0.5f,   0.707f, 0.707f, 0.0f,  0.0f, 0.0f,
+            0.5f, 0.0f,  0.5f,   0.707f, 0.707f, 0.0f,  1.0f, 0.0f,
+            0.0f, 1.0f,  0.0f,   0.707f, 0.707f, 0.0f,  0.5f, 1.0f,
+
+            0.5f, 0.0f,  0.5f,   0.0f, 0.707f, 0.707f,  0.0f, 0.0f,
+            -0.5f, 0.0f,  0.5f,   0.0f, 0.707f, 0.707f,  1.0f, 0.0f,
+            0.0f, 1.0f,  0.0f,   0.0f, 0.707f, 0.707f,  0.5f, 1.0f,
+
+            -0.5f, 0.0f,  0.5f,  -0.707f, 0.707f, 0.0f,  0.0f, 0.0f,
+            -0.5f, 0.0f, -0.5f,  -0.707f, 0.707f, 0.0f,  1.0f, 0.0f,
+            0.0f, 1.0f,  0.0f,  -0.707f, 0.707f, 0.0f,  0.5f, 1.0f
+        };
+
+
+        unsigned int indices[] = {
+            0, 1, 2, 2, 3, 0,
+            4, 5, 6,   
+            7, 8, 9,
+            10,11,12,
+            13,14,15
+        };
+
+
+        unsigned int VAO, VBO, EBO;
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+
+        glBindVertexArray(VAO);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+        glEnableVertexAttribArray(2);
+
+        glBindVertexArray(0);
+
+        return Mesh(VAO, VBO, EBO, sizeof(indices)/sizeof(unsigned int),
+                    glm::vec3(-0.5f, 0.0f, -0.5f),
+                    glm::vec3(0.5f, 1.0f, 0.5f));
+    }
 };
