19cbaef6783134ca0af7746cd325721d1b92225f
quentinbol <quentin.bollore@epitech.eu>
2025-09-08 00:59:39 -0400

    style: Adjust formatting of constructor parameters in Light struct

--- src/ecs/component/LightComponent.hpp
+++ src/ecs/component/LightComponent.hpp
@@ -7,6 +7,6 @@ struct Light {
     u_int lightNumber;
     bool enabled;
 
-    Light(const glm::vec3& c = glm::vec3(1.0f), float i = 1.0f, u_int ln = 0, bool e = true)
+    Light(const glm::vec3 &c = glm::vec3(1.0f), float i = 1.0f, u_int ln = 0, bool e = true)
         : color(c), intensity(i), lightNumber(ln), enabled(e) {}
 };
