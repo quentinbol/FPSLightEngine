4adba7f54bd5aea384d4e52dea97153c26fee4f0
quentinbol <quentin.bollore@epitech.eu>
2025-09-08 00:58:46 -0400

    feat: Add uniform setter methods for vec3, float, and int in Shader component

--- src/ecs/component/ShaderComponent.hpp
+++ src/ecs/component/ShaderComponent.hpp
@@ -73,4 +73,16 @@ struct Shader {
     void setMat4(const std::string &name, const glm::mat4 &mat) const {
         glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
     }
+
+    void setVec3(const std::string &name, const glm::vec3 &value) const {
+        glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+    }
+
+    void setFloat(const std::string &name, float value) const {
+        glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+    }
+
+    void setInt(const std::string &name, int value) const {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+    }
 };
