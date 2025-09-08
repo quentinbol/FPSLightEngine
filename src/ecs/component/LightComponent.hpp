c5eed63a50525d53642ced9b643cc567d229b220
quentinbol <quentin.bollore@epitech.eu>
2025-09-08 00:58:32 -0400

    feat: Add Light component for defining light properties in the ECS

---
+++ src/ecs/component/LightComponent.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <glm/glm.hpp>
+
+struct Light {
+    glm::vec3 color;
+    float intensity;
+    u_int lightNumber;
+    bool enabled;
+
+    Light(const glm::vec3& c = glm::vec3(1.0f), float i = 1.0f, u_int ln = 0, bool e = true)
+        : color(c), intensity(i), lightNumber(ln), enabled(e) {}
+};
